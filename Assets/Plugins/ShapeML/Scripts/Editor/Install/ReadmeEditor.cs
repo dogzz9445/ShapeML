adcbf64b015456ac0f08213756b8fe13bcaa7828
Dongmin Jang (장동민) <37837785+dogzz9445@users.noreply.github.com>
2022-10-10 21:43:49 +0900

    Initialize project

--- Assets/Plugins/ShapeML/Scripts/Editor/Install/ReadmeEditor.cs
+++ Assets/Plugins/ShapeML/Scripts/Editor/Install/ReadmeEditor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace ShapeML.Editor
 {
-    public class Readme : MonoBehaviour
+    public class ReadmeEditor : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
